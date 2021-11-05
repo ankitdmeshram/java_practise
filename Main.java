f8149c47aa17966d96f15b8b56af5c5b5c32e275
Ankit <ankitdm69@gmail.com>
2021-11-05 10:38:51 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,108 @@
+//import java.util.*;
+//public class Main {
+//    public static void main(String[] args) {
+//
+////        Scanner s = new Scanner(System.in);
+//
+////        System.out.print("Hello World");
+//
+////        int no = 10;
+////        int no2 = 11;
+////        String name = "Ankit";
+////        float f = 10.5f;
+////        char l = 'l';
+////        boolean b = false;
+////
+////        System.out.println(no + " " + name);
+////        System.out.println(f + " " + l + " " + b);
+////        System.out.println(no + no2);
+//
+//
+//
+////        type casting
+//
+////        int no = 10;
+////        float f = no;
+//
+////        System.out.println(no);
+////        System.out.println(f);
+////
+////        double d = 23.23d;
+////        int i = (int) d;
+////
+////        String z = s.nextLine();
+////
+////        System.out.println(z);
+////        System.out.println(i);
+//
+//
+////        int a = 10;
+////        int b = 5;
+////        System.out.println(a+b);
+////        System.out.println(a-b);
+////        System.out.println(a*b);
+////        System.out.println(a/b);
+////        System.out.println(a%b);
+////        System.out.println(a++);
+////
+////        System.out.println(a);
+////        System.out.println(a--);
+////        System.out.println(a);
+//
+////
+////        Scanner s = new Scanner(System.in);
+////
+////        int n1 = s.nextInt();
+////        int n2 = s.nextInt();
+////
+////        System.out.println("max = " + Math.max(n1, n2));
+////        System.out.println("min = " + Math.min(n1, n2));
+////        System.out.println("Sqrt = " + Math.sqrt(n1));
+////        System.out.println("Abs= " + Math.abs(n1));
+////        System.out.println("Random = " + Math.random());
+////        System.out.println("Random = " + Math.random()*101);
+//
+////        if(10 > 5)
+////        {
+////            System.out.println("no1 is great");
+////        } else {
+////            System.out.println("no2 is gerat");
+////        }
+////
+////            String x =  (10 < 5) ? "10" : "5";
+////        System.out.println(x);
+//
+//        int i = 0;
+//        do
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
+//        while(i < 10);
+//
+//
+//    }
+//}
+
+import java.util.*;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner s = new Scanner(System.in);
+//        int n;
+//        n = s.nextInt();
+//
+//        for(int i = 0; i < n; i++) {
+//           for(int j = 0; j < n; j++) {
+//               if(j < i) {
+//                   System.out.print(" ");
+//               } else {
+//                   System.out.print("* ");
+//               }
+//           }
+//            System.out.println("");
+//        }
+//    }
+//}
+
---
+++ assignment/assignment.java
@@ -0,0 +1,99 @@
+//Q. 1 ===========================
+//****
+//****
+//****
+//****
+//****
+
+//import java.util.*;
+//public class assignment {
+//    public static void main(String args[])
+//    {
+//        Scanner s = new Scanner(System.in);
+//
+//        int row = s.nextInt();
+//        int col = s.nextInt();
+//
+//        for(int i = 0; i < row; i++)
+//        {
+//            for(int j = 0; j < col; j++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+//
+//    }
+//}
+
+
+//================================================
+
+//2. Hollow Rectangle Pattern
+
+//****
+//*  *
+//*  *
+//*  *
+//****
+//
+//import java.util.Scanner;
+//
+//public class assignment {
+//    public static void main(String args[])
+//    {
+//        Scanner s = new Scanner(System.in);
+//
+//        int row = s.nextInt();
+//        int col = s.nextInt();
+//
+//        for(int i = 0; i < row; i++)
+//        {
+//            for(int j = 0; j < col; j++)
+//            {
+//               if(i == 0 || i == (row-1))
+//               {
+//                   System.out.print("*");
+//               } else {
+//                   if(j == 0 || j == (col-1)){
+//                       System.out.print("*");
+//                   } else {
+//                       System.out.print(" ");
+//                   }
+//               }
+//            }
+//            System.out.println(" ");
+//        }
+//
+//    }
+//}
+
+//==============================================================
+
+//Q.3 Inverted half pyramid
+//*****
+//****
+//***
+//**
+//*
+
+import java.util.Scanner;
+
+public class assignment {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int row = s.nextInt();
+        int col = row;
+
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = col; j > 0; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+            col--;
+        }
+    }
+}
